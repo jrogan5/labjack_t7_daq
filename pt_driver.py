2066042040e40e1ceec86d86c1904c7590e978f3
James Rogan <149914715+jrogan5@users.noreply.github.com>
2025-11-15 20:11:39 -0500

    first commit

---
+++ pt_driver.py
@@ -0,0 +1,8 @@
+#############
+# File       PT Driver
+# Purpose       Interface Pressure transducer with labjack t7 
+# Author        James Rogan
+# Date          2025-11-15
+#############
+
+def read_analog_in(lj, channel):
---
+++ strain_guage_driver.py
@@ -0,0 +1,10 @@
+#############
+# File          strain guage driver
+# Purpose       Interface strain guages with differential PGA with labjack t7 
+# Author        James Rogan
+# Date          2025-11-15
+#############
+
+def read_strain_guuage(lj, pos_ain, neg_ain):
+    # Todo implement
+    
