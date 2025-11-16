7622c797c85b98bd0e112a00b1a0a5f3a9739219
James Rogan <149914715+jrogan5@users.noreply.github.com>
2025-11-15 20:37:15 -0500

    added lua script examples

--- pt_driver.py
+++
@@ -1,8 +0,0 @@
-#############
-# File       PT Driver
-# Purpose       Interface Pressure transducer with labjack t7 
-# Author        James Rogan
-# Date          2025-11-15
-#############
-
-def read_analog_in(lj, channel):
